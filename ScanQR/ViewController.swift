eed8f3e83d61d938e1f6316d69a5762057336a43
mingchenzhang <panxiaorui.666@bytedance.com>
2022-04-15 21:26:28 +0800

    Initial Commit

---
+++ ScanQR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScanQR
+//
+//  Created by XiaorAx on 2022/4/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
